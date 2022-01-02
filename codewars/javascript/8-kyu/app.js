14dc9e8241e501ddea3e67835ed65a71ac5a52e7
Joey <joefrancioso@hotmail.com>
2022-01-01 21:26:24 -0500

    8-kyu find the smallest int arry

--- codewars/javascript/8-kyu/app.js
+++ codewars/javascript/8-kyu/app.js
@@ -1,6 +1,20 @@
 //write a function that turns a positive number into a negative number
-function makeNegative(num) {
-    return -Math.abs(num);
+//function makeNegative(num) {
+   // return -Math.abs(num);
       
+  //  }
+
+//
+
+//find the smallest integer in an array
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      let min = Infinity;
+      for (let i = 0; i < args.length; i++){
+        if (args[i] < min){
+          min = args[i]
+        }
+      }
+      return min;
     }
-    
+  }
